ef94c29720531d11c7b7907d91ea6b5475b7adb7
paddlehands88 <stefancorlett@hotmail.com>
2024-08-07 14:35:00 +0100

    Popular phone brand landing page with animations using GSAP

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://969e3ef4b73e461cfc6a79e9ca3d1546@o4507712729841664.ingest.de.sentry.io/4507712731611216",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
